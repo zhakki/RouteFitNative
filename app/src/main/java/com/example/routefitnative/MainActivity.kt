37914fb56bd2ba7cd8259ec4ad901b4510003d5b
maapin <maapin@taltech.ee>
2026-06-02 22:23:17 +0300

    To make testing easier, I set the MapScreen as the app home screen

--- app/src/main/java/com/example/routefitnative/MainActivity.kt
+++ app/src/main/java/com/example/routefitnative/MainActivity.kt
@@ -1,18 +1,48 @@
 package com.example.routefitnative
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import com.example.routefitnative.ui.screens.login.LoginScreen
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import com.example.routefitnative.ui.screens.MapScreen
 import com.example.routefitnative.ui.theme.RouteFitNativeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContent {
             RouteFitNativeTheme {
-                LoginScreen()
+                val permissionsLauncher = rememberLauncherForActivityResult(
+                    ActivityResultContracts.RequestMultiplePermissions()
+                ) { _ -> }
+
+                LaunchedEffect(Unit) {
+                    val permissions = mutableListOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                        permissions.add(Manifest.permission.ACTIVITY_RECOGNITION)
+                    }
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        permissions.add(Manifest.permission.POST_NOTIFICATIONS)
+                    }
+                    permissionsLauncher.launch(permissions.toTypedArray())
+                }
+
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    // Temporarily setting our functional MapScreen as the home screen
+                    MapScreen()
+                }
             }
         }
     }
